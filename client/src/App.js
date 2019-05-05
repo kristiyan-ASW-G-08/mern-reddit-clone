9a838eee5ed6f048a0919bdf029180abae1df323
Kristiyan Todorov <45337186+krisScript@users.noreply.github.com>
2019-05-05 19:38:52 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React, { Fragment,useState,lazy,Suspense } from 'react';
 import './style/index.scss';
-<<<<<<< HEAD
-import Router from './components/Router'
-=======
+
+
 import Router from './components/Router/Router'
->>>>>>> refactored user routes
 import AuthContext from './AuthContext/AuthContext';
 import ModalContext from './ModalContext/ModalContext';
 const  App  = () => {
